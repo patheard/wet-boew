dda0af41d902080fdce358b36f996f8a9856dec1
Pat Heard <pat.heard@gmail.com>
2013-07-15 13:06:41 -0400

    Expandcollapseall: added accordion option to plugin
    Details elements on a page can now behave like an accordion with
    only one details element open at a time.
    
    * `accordion` CSS class turns on the behaviour.
    * `toggle-none` CSS class disables the creation of toggle buttons.

--- src/js/workers/expandcollapseall.js
+++ src/js/workers/expandcollapseall.js
@@ -11,15 +11,14 @@
 	var _pe = window.pe || {
 		fn : {}
 	};
-
 	_pe.fn.expandcollapseall = {
 		type : 'plugin',
+		_isPolyfill : false,	// Is the browser using the details polyfill?
 		_open : false,			// Globally track the toggle state to support multiple controls on a page
 		_togglers : [],			// Reference to all toggle controls
-		_aria_controls : null,	// Space separated ID list of <details> elements for toggle control aria-controls attribute
+		_aria_controls : null,	// Space separated ID list of details elements for toggle control aria-controls attribute
 		_exec : function (elm) {
-			var mediaQuery,
-				opts,
+			var opts,
 				overrides;
 
 			// Default options
@@ -27,9 +26,11 @@
 				togglers: {			// Define the toggle controls to create
 					toggle: false,	// Toggle open and close
 					open: false,	// Toggle open only
-					close: false	// Toggle close only
+					close: false,	// Toggle close only
+					none: false		// Do not render any toggle controls
 				},
 				accentFirst: false,	// Add 'button-accent' class to first toggle control
+				accordion: false,	// Only allow one <details> element to be open at a time
 				printOpen: false,	// Toggle open before print
 				text: {				// Button text and titles
 					toggle: _pe.dic.get('%td-toggle'),
@@ -45,82 +46,80 @@
 				togglers: {
 					toggle: elm.hasClass('toggle') ? true : false,
 					open: elm.hasClass('toggle-open') ? true : false,
-					close: elm.hasClass('toggle-close') ? true : false
+					close: elm.hasClass('toggle-close') ? true : false,
+					none: elm.hasClass('toggle-none') ? true : false
 				},
 				accentFirst: elm.hasClass('accent-first') ? true : undefined,
+				accordion: elm.hasClass('accordion') ? true : undefined,
 				printOpen: elm.hasClass('print-open') ? true : undefined
 			};
 
 			// Extend the defaults with settings passed through settings.js (wet_boew_expandcollapseall) and class-based overrides
 			$.extend(opts, (typeof wet_boew_expandcollapseall !== 'undefined' ? wet_boew_expandcollapseall : {}), overrides, _pe.data.getData(elm, 'wet-boew'));
 
-			// Create the toggle controls and add them to the page
-			this._initTogglers(elm, opts);
+			this._isPolyfill = _pe.html.hasClass('polyfill-detailssummary');
+
+			// Initialize the togglers unless the user has explicitly asked for none
+			if (opts.togglers.none === false) {
+				this._initTogglers(elm, opts);
+			}
+
+			// Initialize the accordtion
+			if (opts.accordion === true) {
+				this._initAccordion();
+			}
 
 			// Open details on print
-			if(opts.printOpen) {
-
-				// Native event support
-				_pe.window.on('beforeprint', $.proxy(function() {
-					this.setOpen(false);
-					this.toggle();
-				}, this));
-
-				// Fallback for browsers that don't support print event
-				if (typeof window.matchMedia !== 'undefined') {
-					mediaQuery = window.matchMedia('print');
-					if (typeof mediaQuery.addListener !== 'undefined') {
-						mediaQuery.addListener(function(query) {
-							if (query.matches) {
-								_pe.window.trigger('beforeprint');
-							}
-						});
-					}
-				}
-
-				// Polyfill open using CSS
-				$('details').addClass('print-open');
+			if (opts.printOpen === true) {
+				this._initPrint();
 			}
+
 			return elm;
 		}, // end of exec
 
-		isOpen : function() {
-			return this._open === true;
+		isOpen : function () {
+			return this._open;
 		},
 
-		setOpen : function(open) {
+		setOpen : function (open) {
 			this._open = open;
 		},
 
-		toggle : function() {
-			var $details = $('details');
+		toggle : function () {
+			var $details = $('details'),
+				len = this._togglers.length,
+				open = !this.isOpen();
 
-			// Set the state we're currently in and trigger the change
-			$details.prop('open', this.isOpen());
-			$details.find('summary').click();
+			this._changeProp($details, open);
+			this.setOpen(open);
 
-			// Update our state and the title of the toggler controls
-			this.setOpen(!this.isOpen());
-			for(var i = 0, length = this._togglers.length; i < length; i++) {
-				this._setTitle(this._togglers[i]);
+			while (len--) {
+				this._setTitle(this._togglers[len]);
 			}
 		},
 
-		_initTogglers : function(elm, opts) {
+		/**
+		* Initializes the toggle control buttons
+		* @memberof pe.fn.expandcollapseall
+		* @function
+		* @param {jQuery object} elm The object that will hold the toggle control buttons
+		* @param {object} opts Configuration options for the toggle controls
+		*/
+		_initTogglers : function (elm, opts) {
 			var li,
 				toggler,
 				types,
 				ul = document.createElement('ul');
 
 			// Make sure there is at least one toggle control
-			if(!opts.togglers || (!opts.togglers.toggle && !opts.togglers.open && !opts.togglers.close)){
+			if (!opts.togglers || (!opts.togglers.toggle && !opts.togglers.open && !opts.togglers.close)){
 				opts.togglers.toggle = true;
 			}
 
 			// Create the requested togglers and add to the page
 			types = _pe.array.keys(opts.togglers);
 			for(var i = 0, length = types.length; i < length; i++) {
-				if(opts.togglers[types[i]] === true) {
+				if (opts.togglers[types[i]] === true) {
 					toggler = this._createToggler(types[i], opts);
 					li = document.createElement('li');
 					li.appendChild(toggler[0]);
@@ -128,15 +127,113 @@
 					this._togglers.push(toggler);
 				}
 			}
-			ul.className = 'menu-horizontal';
+			ul.className = 'button-group';
 			elm.append(ul);
 
-			if(opts.accentFirst === true) {
-				this._togglers[0].addClass('button-accent');
+			// Accent the first button of the toggler group
+			if (opts.accentFirst === true) {
+				$(ul).find('li:first-child > .button').addClass('button-accent');
 			}
 		},
 
-		_createToggler : function(type, opts) {
+		/**
+		* Initializes the accordion behaviour of details elements:
+		* 1. Only one details open at a time
+		* 2. Nested details clicks do not close their parent details element
+		* @memberof pe.fn.expandcollapseall
+		* @function
+		*/
+		_initAccordion : function () {
+			_pe.document.on('click', 'details summary', function () {
+				var detailClicked = this.parentNode,
+					len,
+					$detail,
+					$details = $('details'),
+					$detailParent = $(detailClicked).parents('details'),
+					$otherDetails;
+
+				// Check for nested details element
+				if ($detailParent.length !== 0) {
+					$otherDetails = $();
+					len = $details.length;
+					while (len--) {
+						$detail = $details.eq(len);
+						// Close nested details elements with the same details parent.	This includes child details elements as well.
+						if ($details[len] !== detailClicked && $detail.parents('details')[0] === $detailParent[0]) {
+							$otherDetails = $otherDetails.add($detail).add($detail.find('details'));
+						}
+					}
+				} else {
+					$otherDetails = $details.not(detailClicked);
+				}
+
+				// Close the other details elements
+				if ($otherDetails.length !== 0) {
+					_pe.fn.expandcollapseall._changeProp($otherDetails, false);
+				}
+			});
+		},
+
+		/**
+		* Initializes the print behaviour of details elements: details open automatically before the page is printed
+		* @memberof pe.fn.expandcollapseall
+		* @function
+		*/
+		_initPrint : function () {
+			var mediaQuery;
+
+			// Native event support
+			_pe.window.on('beforeprint', $.proxy(function () {
+				this.setOpen(false);
+				this.toggle();
+			}, this));
+
+			// Fallback for browsers that don't support 'beforeprint' event
+			if (typeof window.matchMedia !== 'undefined') {
+				mediaQuery = window.matchMedia('print');
+				if (typeof mediaQuery.addListener !== 'undefined') {
+					mediaQuery.addListener(function (query) {
+						if (query.matches) {
+							_pe.window.trigger('beforeprint');
+						}
+					});
+				}
+			}
+
+			// Polyfill open using CSS
+			$('details').addClass('print-open');
+		},
+
+		/**
+		* Changes the open/closed property of details elements.  This causes native and polyfill
+		* details elements to open or close.
+		* @memberof pe.fn.expandcollapseall
+		* @function
+		* @param {jQuery Object} $details Details elements that will have their 'open' property changed.
+		* @param {boolean} open Boolean value of the 'open' property.
+		*/
+		_changeProp : function ($details, open) {
+			$details.prop('open', open);
+			if (this._isPolyfill) {
+				$details.children('summary').attr('aria-expanded', open);
+				if (open) {
+					$details.addClass('open');
+					$details.children(':not(summary)').show();
+				} else {
+					$details.removeClass('open');
+					$details.children(':not(summary)').hide();
+				}
+			}
+		},
+
+		/**
+		* Creates a single toggle control button and bind click event behaviour
+		* @memberof pe.fn.expandcollapseall
+		* @function
+		* @param {string} type The type of toggle control: [open|close|toggle]
+		* @param {object} opts Configuration options for the toggle controls
+		*/
+		_createToggler : function (type, opts) {
 			var $toggler = $('<a>').attr({
 					'href': '#',
 					'role': 'button',
@@ -147,34 +244,51 @@
 					'data-title-open': opts.text.titleOpen
 				}).text(opts.text[type]);
 
-			$toggler.on('click', $.proxy(function(e) {
+			$toggler.on('click', $.proxy(function (event) {
 				this.setOpen(type === 'open' ? false : type === 'close' ? true : this.isOpen());
 				this.toggle();
-				e.preventDefault();
-				e.target.focus();
+				event.preventDefault();
+				event.target.focus();
 			}, this));
 
 			this._setTitle($toggler);
 			return $toggler;
 		},
 
-		_getAriaControls : function() {
-			var ids = '';
+		/**
+		* Creates the space separated list of details 'id' attributes.	Used by the 'aria-controls' attribute of the toggle button(s).
+		* @memberof pe.fn.expandcollapseall
+		* @function
+		* @return {string} Space separated list of details 'id' attributes.
+		*/
+		_getAriaControls : function () {
+			var detail,
+				details,
+				ids = '',
+				len;
 
-			// Init with a space separated list of <details> element IDs
-			if(this._aria_controls === null) {
-				$('details').each(function(idx) {
-					if(this.id === '') {
-						this.id = 'details_' + idx;
+			if (this._aria_controls === null) {
+				details = document.getElementsByTagName('details');
+				len = details.length;
+				while (len--) {
+					detail = details[len];
+					if (detail.id === '') {
+						detail.id = 'details_' + len;
 					}
-					ids += this.id + ' ';
-				});
-				this._aria_controls = $.trim(ids);
+					ids += detail.id + ' ';
+				}
+				this._aria_controls = ids.slice(0, -1);
 			}
 			return this._aria_controls;
 		},
 
-		_setTitle : function(toggler) {
+		/**
+		* Updates the title attribute of a given toggle control element.  Title is based on the type of toggle control and the current open/closed state of the plugin.
+		* @memberof pe.fn.expandcollapseall
+		* @function
+		* @return {jQuery object} toggler The toggle control that needs its title attribute updated
+		*/
+		_setTitle : function (toggler) {
 			var type = toggler.data('type');
 			toggler[0].title = type === 'close' || (type !== 'open' && this.isOpen()) ? toggler.data('title-close') : toggler.data('title-open');
 		}
